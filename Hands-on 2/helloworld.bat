788a7f5c7df9e0ad2321a0e6dfaf3b0d7fd1512c
Pepisxd <112013480+Pepisxd@users.noreply.github.com>
2024-10-07 00:19:09 -0600

    Add files via upload

---
+++ Hands-on 2/helloworld.bat
@@ -0,0 +1,23 @@
+:: Imprimimos  "Hello World"
+echo Hello World
+
+:: Listar el contenido del directorio actual.
+echo Lista de el contenido del directorio actual:
+dir
+
+:: Crear subdirectorio llamado "Test"
+echo Creando el directorio actual:
+mkdir Test
+
+:: Cambiar al subdirectorio "Test"
+echo Cambiar al subdirectorio "Test"
+cd Test
+
+:: Listar el contenido de "Test"
+echo Listando el contenido del subdirectorio Test:
+dir
+
+:: Fin del script
+pause
+
+

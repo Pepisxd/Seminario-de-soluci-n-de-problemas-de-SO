bbfa24708bf967d65c74afbef41532a4776d43c0
Pepisxd <112013480+Pepisxd@users.noreply.github.com>
2024-10-07 00:40:58 -0600

    Add files via upload

---
+++ Hands-on 3/helloworld.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Imprimir en pantalla "Hello World"
+echo "Hello World"
+
+# Listar el contenido del directorio actual
+echo "Listado del directorio actual:"
+ls
+
+# Crear un directorio llamado Test
+mkdir Test
+
+# Cambiarse al directorio Test
+cd Test
+
+# Listar el contenido del directorio Test
+echo "Listado del directorio Test:"
+ls
